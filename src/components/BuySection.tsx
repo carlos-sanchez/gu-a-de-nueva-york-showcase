055eeaad7f4cc5c32ae0d6906d47143097c15554
Carlos Sánchez <hi@carlossanchez.me>
2025-12-21 21:38:45 +0100

    Update BuySection.tsx

--- src/components/BuySection.tsx
+++ src/components/BuySection.tsx
@@ -12,7 +12,7 @@ const buyOptions = [
   },
   {
     title: "Gigantes.com",
-    description: "La tienda de la mítica revista de basket",
+    description: "Versión física con envío a toda España",
     type: "Físico",
     icon: ShoppingBag,
     url: "https://www.gigantes.com/tienda/libros-baloncesto/guia-de-nueva-york/",
